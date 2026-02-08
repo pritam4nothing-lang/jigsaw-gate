af631aaa8c0a8cb7684de3ced5b5eedfa36117e7
pritam4nothing-lang <pritam4nothing@gmail.com>
2026-02-08 22:02:10 +0530

    Update generator.js

--- js/generator.js
+++ js/generator.js
@@ -3,97 +3,14 @@ function generateEdgeMatrix(rows, cols) {
 
   for (let r = 0; r < rows; r++) {
     matrix[r] = [];
-
     for (let c = 0; c < cols; c++) {
-      const piece = {
-        top: 0,
-        right: 0,
-        bottom: 0,
-        left: 0
+      matrix[r][c] = {
+        top: r === 0 ? 0 : -matrix[r - 1][c].bottom,
+        left: c === 0 ? 0 : -matrix[r][c - 1].right,
+        right: c === cols - 1 ? 0 : Math.random() > 0.5 ? 1 : -1,
+        bottom: r === rows - 1 ? 0 : Math.random() > 0.5 ? 1 : -1
       };
-
-      // TOP
-      if (r === 0) {
-        piece.top = 0;
-      } else {
-        piece.top = -matrix[r - 1][c].bottom;
-      }
-
-      // LEFT
-      if (c === 0) {
-        piece.left = 0;
-      } else {
-        piece.left = -matrix[r][c - 1].right;
-      }
-
-      // RIGHT
-      if (c === cols - 1) {
-        piece.right = 0;
-      } else {
-        piece.right = Math.random() > 0.5 ? 1 : -1;
-      }
-
-      // BOTTOM
-      if (r === rows - 1) {
-        piece.bottom = 0;
-      } else {
-        piece.bottom = Math.random() > 0.5 ? 1 : -1;
-      }
-
-      matrix[r][c] = piece;
     }
   }
-
   return matrix;
-}
-function createHorizontalEdgePath(path, x, y, size, type) {
-  const tabWidth = size * 0.3;
-  const tabHeight = size * 0.25 * type; // + for head, - for socket
-
-  const startX = x;
-  const endX = x + size;
-  const midX = x + size / 2;
-
-  // Straight line to start of tab
-  path.lineTo(midX - tabWidth / 2, y);
-
-  if (type !== 0) {
-    // Bézier curve for tab
-    path.bezierCurveTo(
-      midX - tabWidth / 2,
-      y - tabHeight,
-      midX + tabWidth / 2,
-      y - tabHeight,
-      midX + tabWidth / 2,
-      y
-    );
-  }
-
-  // Straight line to end
-  path.lineTo(endX, y);
-}
-function createVerticalEdgePath(path, x, y, size, type) {
-  const tabWidth = size * 0.3;
-  const tabHeight = size * 0.25 * type;
-
-  const startY = y;
-  const endY = y + size;
-  const midY = y + size / 2;
-
-  // Straight line to start of tab
-  path.lineTo(x, midY - tabWidth / 2);
-
-  if (type !== 0) {
-    path.bezierCurveTo(
-      x - tabHeight,
-      midY - tabWidth / 2,
-      x - tabHeight,
-      midY + tabWidth / 2,
-      x,
-      midY + tabWidth / 2
-    );
-  }
-
-  // Straight line to end
-  path.lineTo(x, endY);
 }

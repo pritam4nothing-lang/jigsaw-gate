5eeae2c19829277bbe7d00f1b0f25cca87696155
pritam4nothing-lang <pritam4nothing@gmail.com>
2026-02-08 16:20:28 +0530

    Create generator.js

---
+++ js/generator.js
@@ -0,0 +1,48 @@
+function generateEdgeMatrix(rows, cols) {
+  const matrix = [];
+
+  for (let r = 0; r < rows; r++) {
+    matrix[r] = [];
+
+    for (let c = 0; c < cols; c++) {
+      const piece = {
+        top: 0,
+        right: 0,
+        bottom: 0,
+        left: 0
+      };
+
+      // TOP
+      if (r === 0) {
+        piece.top = 0;
+      } else {
+        piece.top = -matrix[r - 1][c].bottom;
+      }
+
+      // LEFT
+      if (c === 0) {
+        piece.left = 0;
+      } else {
+        piece.left = -matrix[r][c - 1].right;
+      }
+
+      // RIGHT
+      if (c === cols - 1) {
+        piece.right = 0;
+      } else {
+        piece.right = Math.random() > 0.5 ? 1 : -1;
+      }
+
+      // BOTTOM
+      if (r === rows - 1) {
+        piece.bottom = 0;
+      } else {
+        piece.bottom = Math.random() > 0.5 ? 1 : -1;
+      }
+
+      matrix[r][c] = piece;
+    }
+  }
+
+  return matrix;
+}
